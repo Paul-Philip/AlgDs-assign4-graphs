e3c6ebff7fe6fa98edab70c4cd73c67ddfeea90f
Paul-Philip <paulphilip.mosulet@gmail.com>
2016-09-20 14:58:16 +1000

    Sketched out readEdges function, changed some variable-names

--- CamDetector.py
+++ CamDetector.py
@@ -1,7 +1,8 @@
+#!/usr/bin/env python
 
 
-def readnext(camerasIndices):
-    cameraIndex = camerasIndices.readline().split(" ")[0]
+def readnext(cameraIndices):
+    cameraIndex = cameraIndices.readline().split(" ")[0]
     if cameraIndex == '':
         return -1
     else:
@@ -9,28 +10,41 @@ def readnext(camerasIndices):
 
 
 def readVertices():
-    camerasIndices = open('vertices.txt','r')
-    currentCameraIndex = readnext(camerasIndices)
+    cameraIndices = open('vertices.txt', 'r')
+    currentCameraIndex = readnext(cameraIndices)
     x = 0
     vertices = []
     while x <= 6104:
         if currentCameraIndex == x:
             vertices.append(1)
-            currentCameraIndex = readnext(camerasIndices)
+            currentCameraIndex = readnext(cameraIndices)
         else:
             vertices.append(0)
         x += 1
     return vertices
 
 
+def addEdge(vert1, vert2, weight):
+    # TODO adding edges for both vertices
+    return 0
+
+
 def readEdges():
+    # TODO reading edges
     edges = []
+    # To check if "TOLL":
+    # try:
+    #     edgesInput[3] #the index of "TOLL" text...
+    #     no error? aka there is a TOLL aka. don't add to adjacency list.
+    # except IndexError:
+    #     no TOLL add edge to adjacency list.
     return edges
 
 
 def main():
     vertices = readVertices()
     edges = readEdges()
+
 
 if __name__ == '__main__':
     main()
