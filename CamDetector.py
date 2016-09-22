ec770beafc2084bfe5b0c380aba3c9811aa86070
Paul-Philip <paulphilip.mosulet@gmail.com>
2016-09-23 00:53:46 +1000

    My heap for dijkstras implemented, dijkstras implemented, getting a list with all reachable cameras.

--- CamDetector.py
+++ CamDetector.py
@@ -1,49 +1,59 @@
-#!/usr/bin/env python
+# Author:   Paul-Philip Mosulet
+# Date:     23/9-2016
+
+import reading_input
+from my_dijkstra_heap import pop
+from my_dijkstra_heap import push
+from my_dijkstra_heap import update_distance
 
 
-def readnext(cameraIndices):
-    cameraIndex = cameraIndices.readline().split(" ")[0]
-    if cameraIndex == '':
-        return -1
-    else:
-        return int(cameraIndex)
+def cam_detector(cameras, adjacency_list, start, k):
+    """
+    Uses djikstras algorithm to find and print
+    the k closest cameras. With out passing through
+    any other cameras or tolled roads.
+    """
+    if cameras[start] == 1:
+        print "Oops! Too late to help!!! Please smile for the camera!"
+        return
+    heap = [(start, 0)]
+    finalized = []
+    vertices = [-2 for x in range(0, 6105)]
+    vertices[start] = 0
+    red_lights = []
+    while heap != []:
+        for edge in adjacency_list[heap[0][0]]:
+            index_in_heap = vertices[edge[0]]
+            new_distance = heap[0][1] + edge[1]
+            if cameras[edge[0]] == 1:       # camera found!
+                red_lights.append((edge[0],new_distance))
+            elif index_in_heap == -2:       # non discovered vertex
+                push(heap, edge[0], new_distance, vertices)
+            elif index_in_heap > 0 and heap[index_in_heap][1] > new_distance:
+                # if distance should be updated
+                update_distance(heap, edge[0], new_distance, vertices)
+        vertex = pop(heap, vertices)
+        finalized.append(vertex)
+        vertices[vertex[0]] = -1
 
-
-def readVertices():
-    cameraIndices = open('vertices.txt', 'r')
-    currentCameraIndex = readnext(cameraIndices)
-    x = 0
-    vertices = []
-    while x <= 6104:
-        if currentCameraIndex == x:
-            vertices.append(1)
-            currentCameraIndex = readnext(cameraIndices)
-        else:
-            vertices.append(0)
-        x += 1
-    return vertices
-
-
-def addEdge(vert1, vert2, weight):
-    # TODO adding edges for both vertices
-    return 0
-
-
-def readEdges():
-    # TODO reading edges
-    edges = []
-    # To check if "TOLL":
-    # try:
-    #     edgesInput[3] #the index of "TOLL" text...
-    #     no error? aka there is a TOLL aka. don't add to adjacency list.
-    # except IndexError:
-    #     no TOLL add edge to adjacency list.
-    return edges
+    # TODO make it print the k closest vertices only.
+    for x in red_lights:
+        print x
 
 
 def main():
-    vertices = readVertices()
-    edges = readEdges()
+    """
+    Reads in vertices with red-light cameras and edges without a toll.
+    Reads input values start and k.
+    Then performs a dijkstras from the vertice start using my min heap.
+    to find the k closest red-light camera.
+    :return:
+    """
+    cameras = reading_input.read_vertices()
+    adjacency_list = reading_input.read_edges()
+    start, k = reading_input.user_input()
+
+    cam_detector(cameras, adjacency_list, start, k)
 
 
 if __name__ == '__main__':
---
+++ reading_input.py
@@ -0,0 +1,75 @@
+# Author:   Paul-Philip Mosulet
+# Date:     23/9-2016
+
+def read_next(vertices_input):
+    """
+    :return: next index of camera or -1 if no more cameras present.
+    """
+    camera_index = vertices_input.readline().split(" ")[0]
+    if camera_index == '':   # done reading in.
+        return -1
+    else:
+        return int(camera_index)
+
+
+def read_vertices():
+    """
+    Reads in red-light cameras from 'vertices.txt' on the form:
+    <index> "camera"
+    :return: array with vertices marked 1 - camera, 0 - no camera
+    """
+    vertices_input = open('vertices.txt', 'r')
+    current_camera_index = read_next(vertices_input)
+    x = 0
+    vertices = []
+    while x <= 6104:
+        if current_camera_index == x:
+            vertices.append(1)
+            current_camera_index = read_next(vertices_input)
+        else:
+            vertices.append(0)
+        x += 1
+    return vertices
+
+
+def add_edge(vertice_1, vertice_2, weight, adjacency_list):
+    """
+    Adds the edge to the adjacency list
+    """
+    adjacency_list[int(vertice_1)].append((int(vertice_2), float(weight)))
+    adjacency_list[int(vertice_2)].append((int(vertice_1), float(weight)))
+
+
+def read_edges():
+    """
+    Reads the input edges from edges.txt on the format:
+    <vertice1> <vertice2> <weight> "TOLL"(optional)
+
+    Where the vertices are in range 0-6104.
+    :return: adjacency list representation of the graph.
+    """
+
+    edges_input = open('edges.txt', 'r')
+    adjacency_list = [[] for x in range(0,6105)]
+
+    for edge in edges_input:
+        edge_info = edge.split(" ")
+        try:
+            edge_info[3]             # Check if there is a TOLL.
+        except IndexError:          # !TOLL catch the Index Error insert edge.
+            add_edge(edge_info[0], edge_info[1], edge_info[2], adjacency_list)
+
+    return adjacency_list
+
+
+def user_input():
+    start = input("Enter your location: ")
+    k = input("Enter k: ")
+    return start, k
+
+
+def main():
+    return
+
+if __name__ == '__main__':
+    main()
